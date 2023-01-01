72c2d71462d72f7a043c1508773e8059cdba7e44
albe9 <alber@Albe-pc.mshome.net>
2023-01-02 00:47:50 +0100

    File con variabili globali aggiunto

---
+++ Interlocking_system/includes/global_variables.h
@@ -0,0 +1,19 @@
+/*
+ * global_variables.h
+ *
+ *  Created on: Jan 2, 2023
+ *      Author: alber
+ */
+
+//Variabili globali comuni tra tutti i task (es ID task e semafori)
+
+#ifndef INCLUDES_GLOBAL_VARIABLES_H_
+#define INCLUDES_GLOBAL_VARIABLES_H_
+
+#include "taskLib.h"
+#include "semLib.h"
+
+
+SEM_ID SEM_LOG;
+
+#endif /* INCLUDES_GLOBAL_VARIABLES_H_ */
