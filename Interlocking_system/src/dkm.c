51a4d835ad377352a1b0a92ad57125611a652ff8
albe9 <alber@Albe-pc.mshome.net>
2023-01-04 19:23:31 +0100

    Aggiunta la routine per il wifiTask

--- Interlocking_system/src/dkm.c
+++ Interlocking_system/src/dkm.c
@@ -4,12 +4,12 @@
 #include <stdio.h>
 #include <errno.h>
 #include <taskLib.h>
-#include <arpa/inet.h>
-#include <sockLib.h>
+
 
 #include "global_variables.h"
 #include "initTask.h"
 #include "logTask.h"
+#include "wifiTask.h"
 
 
 TASK_ID taskId;
@@ -50,101 +50,7 @@ void start3(void){
 
 */
 
-void wifiRoutine(void){
-	
-	int PORT = 6543;
-	char HOST[] = "192.168.1.202";
-	
-	int sock = 0, valread, client_fd;
-	struct sockaddr_in serv_addr;
-	
-	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-		printf("\n Socket creation error \n");
-		perror("Errore ");
-	}
-	
-	serv_addr.sin_addr.s_addr = inet_addr(HOST);
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(PORT);
- 
- 
-	if ((client_fd = connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) < 0) {
-		printf("\nConnection Failed \n");
-		perror("Errore ");
-	}
-	
-	char* hello = "Hello from client";
-	char buffer[1024] = { '\0' };
-	send(sock, hello, strlen(hello), 0);
-	printf("Hello message sent\n");
-	
-	valread = read(sock, buffer, 1024);
-	printf("Messaggio ricevuto : %s\n", buffer);
-	
-	// closing the connected socket
-	close(client_fd);
-	
-}
 
-void wifiServerRoutine(void){
-	int sock, new_sock, addrlen;
-	struct sockaddr_in server, client;
-	
-	
-	
-	//Create socket
-	
-	if ((sock = socket(AF_INET , SOCK_STREAM , 0)) < 0)
-	{
-		printf("\nImpossibile creare il socket\n");
-		perror("Errore ");
-	}
-	
-	//Set sock option
-	
-	int reuse_addr = 1;
-	if (setsockopt(sock,SOL_SOCKET,SO_REUSEADDR,&reuse_addr,sizeof(int)) < 0)
-	{
-		perror("Setsockopt");
-	}
-	
-		
-	//Prepare the sockaddr_in structure
-	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = INADDR_ANY;
-	server.sin_port = htons( 6543 );
-		
-	//Bind
-	
-	if( bind(sock,(struct sockaddr *)&server , sizeof(server)) < 0)
-	{
-		perror("Errore durante il Bind");
-	}
-	printf("Bind eseguito");
-	
-	//listen for connection
-	listen(sock , 3);
-	//Accept and incoming connection
-	printf("\nIn attesa di connessioni\n");
-	addrlen = sizeof(struct sockaddr_in);
-	if ((new_sock = accept(sock, (struct sockaddr *)&client, (socklen_t*)&addrlen)) < 0)
-	{
-		perror("accept failed");
-	}
-	
-	printf("\nConnessione accettata\n");
-	
-	char host_addr[INET_ADDRSTRLEN];
-	inet_ntop(AF_INET, &(client.sin_addr), host_addr, INET_ADDRSTRLEN);
-	printf("\n%s\n",host_addr);
-	
-	close(new_sock);
-	shutdown(sock, SHUT_RDWR);
-	if (close(sock) < 0){
-		perror("Errore chiusura server socket");
-	}
-	printf("\nFine connessione\n");
-}
 
 void startLog(void){
 	LOG_TID = taskSpawn("LogTask", 100, 0, 20000,(FUNCPTR) logInit, 0,0,0,0,0,0,0,0,0,0);
@@ -155,26 +61,11 @@ void test(void){
 }
 
 void startInit(void){
-	INIT_TID = taskSpawn("tTest", 10, 0, 20000,(FUNCPTR) initMain, 0,0,0,0,0,0,0,0,0,0);
+	INIT_TID = taskSpawn("tTest", 50, 0, 20000,(FUNCPTR) initMain, 0,0,0,0,0,0,0,0,0,0);
 }
 
-void setTime(void){
-	taskSpawn("SetTimeTask", 1, 0, 20000,(FUNCPTR) setCurrentTime, 0,0,0,0,0,0,0,0,0,0);
+void startWifi(void){
+	WIFI_TID = taskSpawn("wifi_task", 50, 0, 20000,(FUNCPTR) wifiMain, 0,0,0,0,0,0,0,0,0,0);
 }
 
-void myTd(char * task_to_delete){
-	printf("\n%s\n",task_to_delete);
-	TASK_ID tid_to_delete = taskNameToId(task_to_delete);
-	printf("task id : %i\n",tid_to_delete);
-	taskDeleteForce(tid_to_delete);
-}
-
-
-void wifi(void){
-	taskId = taskSpawn("wifi_task", 1, 0, 20000,(FUNCPTR) wifiRoutine, 0,0,0,0,0,0,0,0,0,0);
-}
-
-void wifiServer(void){
-	taskId = taskSpawn("wifiServer_task", 1, 0, 20000,(FUNCPTR) wifiServerRoutine, 0,0,0,0,0,0,0,0,0,0);
-}
 
