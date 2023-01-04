3988d82cbc306dfa8593dbbe93605e98d1951901
albe9 <alber@Albe-pc.mshome.net>
2023-01-04 17:27:48 +0100

    Aggiunta la coda dei log

--- Interlocking_system/src/logTask.c
+++ Interlocking_system/src/logTask.c
@@ -2,54 +2,35 @@
 //Gestisce la scrittura su un file di log tramite una funzione utilizzabile dai vari task
 
 #include "logTask.h"
-#include <sys/time.h>
 
+#define MAX_LOG_SIZE 1024
+#define MAX_LOG_BUFF 3
 
+SEM_ID SEM_LOG_FILE;
+SEM_ID SEM_LOG_MSG;
 int LOG_FD;
+BOOL FLAG_LOG_ACTIVE = FALSE;
+
+struct string_queue{
+	char buffer[MAX_LOG_BUFF][MAX_LOG_SIZE];
+	int log_number;
+};
+struct string_queue log_buffer;
 
 
 void mytest(void){
 	
-	
-
+	logMessage("Messaggio log di prova 1", taskName(LOG_TID));
+	logMessage("Messaggio log di prova 2", taskName(LOG_TID));
+	logMessage("Messaggio log di prova 3", taskName(LOG_TID));
 }
 
-void setCurrentTime(void){
-	struct tm current_time;
-	current_time.tm_sec  = 0;				//seconds after the minute – [0, 61] (until C99)[0, 60]
-	current_time.tm_min  = 37;				//minutes after the hour – [0, 59]
-	current_time.tm_hour = 20;				//hours since midnight – [0, 23]
-	current_time.tm_mday = 2;				//day of the month – [1, 31]
-	current_time.tm_mon  = 1 - 1;			//months since January – [0, 11]
-	current_time.tm_year = 2023 - 1900;		//years since 1900
-	current_time.tm_wday = 0;				//days since Sunday – [0, 6]
-	current_time.tm_yday = 1;				//days since January 1 – [0, 365]
-	
-
-	time_t current_time_clike = mktime(&current_time);
-	struct timeval current_timeval;
-	current_timeval.tv_sec = current_time_clike;
-	current_timeval.tv_usec = 0;
-	
-	
-	if(settimeofday(&current_timeval, NULL) < 0){
-		perror("Errore nel settare il tempo :");
-	}
-	
-	
-	time_t rawtime;
-	struct tm * timeinfo;
-	
-	time (&rawtime);
-	timeinfo = localtime (&rawtime);
-	printf ("Current local time and date: %s", asctime(timeinfo));	
-}
 
 
 //Funzione che scrive i messaggi sul file di log, gestisce l'accesso concorrente al file con un semaforo 
 
 void logMessage(char* msg, char* task_name){
-	char final_msg[1024] = "task : ";
+	char final_msg[MAX_LOG_SIZE] = "task : ";
 	
 	//TODO inserire controllo su lunghezza messaggio
 	
@@ -57,6 +38,7 @@ void logMessage(char* msg, char* task_name){
 	//Salvo il timestamp
 	time_t rawtime;
 	struct tm * timeinfo;
+	
 	char timestamp[80];
 	
 	time(&rawtime);
@@ -70,28 +52,39 @@ void logMessage(char* msg, char* task_name){
 	strcat(final_msg, msg);	
 	strcat(final_msg, "\n");
 	
-	//acquisisco il semaforo e scrivo sul file
-	if(semTake(SEM_LOG, WAIT_FOREVER) == ERROR){
-		perror("\nErrore nell'acquisire semaforo di log");
+	//acquisisco il semaforo e scrivo sul buffer
+	if(semTake(SEM_LOG_FILE, WAIT_FOREVER) == ERROR){
+		perror("\nErrore nell'acquisire semaforo del file di log");
 		return;
 	}
 	
-	if(write(LOG_FD, final_msg, strlen(final_msg)) < 0){
-		perror("\nErrore nella scrittura di un log:");
+	if(FLAG_LOG_ACTIVE == FALSE){
+		perror("Log task non attivo");
+		return;
 	}
+	
+	//controllo se c'è spazio e aggiungo il messaggio alla coda
+	if(log_buffer.log_number < MAX_LOG_BUFF){
+		strcpy(log_buffer.buffer[log_buffer.log_number], final_msg);
+		log_buffer.log_number++;
+		
+		semGive(SEM_LOG_MSG);
+	}
+	
 	//rilascio il semaforo
-	if(semGive(SEM_LOG) == ERROR){
-		perror("\nErrore nel rilasciae il semaforo di log");
+	if(semGive(SEM_LOG_FILE) == ERROR){
+		perror("\nErrore nel rilasciare il semaforo del file di log");
 		return;
 	}
 	
+	semGive(SEM_LOG_MSG);
 	
 }
 
 
 
 void logInit(void){
-	//Crea file di log
+	//Crea la directory e il file di log se non sono già esistenti
 	
 	struct stat st = {0};
 	//Controlla se esiste directory di log, altrimenti la crea
@@ -100,18 +93,58 @@ void logInit(void){
 	    	perror("\nErrore creazione directory di log:");
 	    }
 	}
-	//Crea il file di log
+	//Crea/apre il file di log in modalità append
 	if ((LOG_FD = open("/usr/log/log.txt",O_RDWR | O_APPEND  | O_CREAT, 00700)) < 0){
 		perror("\nErrore apertura file di log");
 	}
 	
-	//creo il semaforo per regolare l'accesso al file di log
-	SEM_LOG = semBCreate(SEM_Q_FIFO, SEM_FULL);
+	//creo il semaforo per regolare l'accesso al file e per notificare l'arrivo di msg di log
+	SEM_LOG_FILE = semBCreate(SEM_Q_FIFO, SEM_FULL);
+	SEM_LOG_MSG = semBCreate(SEM_Q_FIFO, SEM_EMPTY);
 	
-	logMessage("Messaggio log di prova", taskName(LOG_TID));
 	
+	//inizializzo la coda per il buffer e abilito la funzione logMessage(tramite FLAG_LOG_ACTIVE)
+	log_buffer.log_number = 0;
+	FLAG_LOG_ACTIVE = TRUE;
 	
-	//test
-	close(LOG_FD);
+	//main loop del task, controlla la coda dei messaggi di log e li scrive su file
+	while(TRUE){
+		
+		//mi metto in attesa di un messaggio
+		if(semTake(SEM_LOG_MSG, WAIT_FOREVER) == ERROR){
+			perror("\nErrore nell'acquisire semaforo dei messaggi di log");
+			taskDelete(LOG_TID);
+		}
+		//acquisisco il semaforo e scrivo sul file
+		if(semTake(SEM_LOG_FILE, WAIT_FOREVER) == ERROR){
+			perror("\nErrore nell'acquisire semaforo del file di log");
+			taskDelete(LOG_TID);
+		}
+		
+		//scrivo tutti i messaggi presenti in coda sul file di log
+		
+		
+		for(int log_idx = 0;log_idx < log_buffer.log_number;log_idx++){
+			if(write(LOG_FD, log_buffer.buffer[log_idx], strlen(log_buffer.buffer[log_idx])) < 0){
+				perror("\nErrore nella scrittura di un log:");
+				taskDelete(LOG_TID);
+			}
+			
+		}
+		log_buffer.log_number = 0;
+			
+		
+		//rilascio il semaforo
+		if(semGive(SEM_LOG_FILE) == ERROR){
+			perror("\nErrore nel rilasciare il semaforo del file log");
+			taskDelete(LOG_TID);
+		}
+	}
 	
-}
+}	
+
+
+
+
+
+
