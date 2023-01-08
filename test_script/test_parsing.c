510f723fdee2cd933695040cd3da34a868dbb7cc
albe9 <albertom1999@gmail.com>
2023-01-08 20:22:23 +0100

    test in corso

--- test_script/test_parsing.c
+++ test_script/test_parsing.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
+#include <string.h>
 
-void main(){
+/*
+    test string: 
+    Time:1673204636,Route:a,rasp_id_prev:0,rasp_id_next:2,prev_ip:127.0.0.1,next_ip:127.0.0.1,Stop:1
 
-    printf("PROVA");
-    
+*/
+
+int main(){
+
+    char test_msg[] = "Time:1673204636,Route:a,rasp_id_prev:0,rasp_id_next:2,prev_ip:127.0.0.1,next_ip:127.0.0.1,Stop:1"; 
+    char *token;
+    char *key;
+    token = strtok(test_msg, ",");
+
+    while(token != NULL){
+        printf("%s\n",token);
+        token = strtok(NULL, ",");
+        
+        key = strtok(token, ":");
+        while(key != NULL){
+            printf("%s\n",key);
+            key = strtok(NULL, ",");
+            
+        }
+    }
+
+    return(0);
     
 }
