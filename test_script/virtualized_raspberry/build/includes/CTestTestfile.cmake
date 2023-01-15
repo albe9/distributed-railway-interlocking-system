1a6bb29027cf6ec409f2449d7e8d79017ed022de
albe9 <albertom1999@gmail.com>
2023-01-15 21:44:00 +0100

    Rimossa copia del cmake

--- test_script/virtualized_raspberry/build/includes/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/albe/distributed-railway-interlocking-system/test_script/virtualized_raspberry/includes
-# Build directory: /home/albe/distributed-railway-interlocking-system/test_script/virtualized_raspberry/build/includes
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- test_script/virtualized_raspberry/build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/albe/distributed-railway-interlocking-system/test_script/virtualized_raspberry/src
-# Build directory: /home/albe/distributed-railway-interlocking-system/test_script/virtualized_raspberry/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
