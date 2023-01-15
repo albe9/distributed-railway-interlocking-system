78d85c388f0d0204158f7920e3a03764ef075567
albe9 <albertom1999@gmail.com>
2023-01-15 23:39:24 +0100

    Revert "Delete build directory"
    This reverts commit 6f3db3b0d4f14e0b385f2ea26a0c42b557b38312.

---
+++ build/CMakeFiles/Virtualized_raspberry.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Virtualized_raspberry.dir/main.c.o"
+  "CMakeFiles/Virtualized_raspberry.dir/main.c.o.d"
+  "Virtualized_raspberry"
+  "Virtualized_raspberry.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Virtualized_raspberry.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/wifi.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wifi.dir/src/wifiTask.c.o"
+  "CMakeFiles/wifi.dir/src/wifiTask.c.o.d"
+  "libwifi.a"
+  "libwifi.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/wifi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
