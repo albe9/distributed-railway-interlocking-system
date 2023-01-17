cd78e54bbbcea6e514d47668a61f32fd22130724
albe9 <albertom1999@gmail.com>
2023-01-17 16:58:49 +0100

    Creati due script per eseguire l'host e i nodi in due shell separate

---
+++ test_script/virtualized_raspberry/launch_host.sh
@@ -0,0 +1,1 @@
+python3 ../../host_script/host.py
--- test_script/virtualized_raspberry/launch_nodes.sh
+++ test_script/virtualized_raspberry/launch_nodes.sh
@@ -3,7 +3,7 @@
 #base ip addr : 172.23.78.0
 N_IP=2
 #aggiungo N_IP all'interfaccia di rete eth0
-for ((label=1; label<=$N_IP; label++))
+for ((label=0; label<=$N_IP; label++))
 do
     ip=$(ip address show dev eth0 label eth0:$label)
     if [ -z "$ip" ]
@@ -15,6 +15,9 @@ done
 # #cambia directory e compila 
 (cd ../../build && make)
 
-# #esegue in parallelo i file compilati con i relativi args
-../../build/Virtualized_raspberry "172.23.78.1" 1 &
-../../build/Virtualized_raspberry "172.23.78.2" 2
+
+
+# esegue i file compilati con i relativi args
+
+../../build/Virtualized_raspberry "172.23.78.1" 1
+# ../../build/Virtualized_raspberry "172.23.78.2" 2
