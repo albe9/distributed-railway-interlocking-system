169beed28a2fc6d564528053fa82009349b3b4f6
albe9 <albertom1999@gmail.com>
2023-01-17 14:40:22 +0100

    Modificato main per accettare ip e rasp id in input da riga di comando

--- test_script/virtualized_raspberry/main.c
+++ test_script/virtualized_raspberry/main.c
@@ -3,10 +3,21 @@
 #include "initTask.h"
 
 
-int main(){
+int main(int argc, char *argv[]){
 
-    initMain();
-   
+    if(argc > 1){
+        int rasp_id = -1;
+        char rasp_ip[20];
+        
+        strcpy(rasp_ip,argv[1]);    
+        rasp_id = atoi(argv[2]);
+        
+        printf("ip : %s id : %i\n",rasp_ip,rasp_id);
+    }
+    
+
+    //initMain();
+
     return(0);
     
 }
