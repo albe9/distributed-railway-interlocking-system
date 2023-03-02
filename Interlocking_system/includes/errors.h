d1d4e06bd39be4bb1b1895153cb42c9b7ea6847e
albe9 <albertom1999@gmail.com>
2023-03-02 18:49:32 +0100

    Creato file c associato (forse non necessario)

--- Interlocking_system/includes/errors.h
+++ Interlocking_system/includes/errors.h
@@ -24,22 +24,6 @@ enum _EXIT_NUMBER {
 
 typedef enum _EXIT_NUMBER exit_number;
 
-const char *errorDescription(exit_number exit_number) {
-    switch(exit_number) {
-        case E_DEFAUL_ERROR: return strerror(errno); break;
-        case E_SUCCESS: return "OK"; break;
-        case E_INVALID_ID: return "INVALID CONNECTION SERVER ID"; break;
-        case E_MAX_CONNECTION_NUMBER: return "REACHED THE MAXIMUM NUMBER OF CONNECTIONS"; break;
-        case E_CONN_REFUSED: return "UNABLE TO CONNECT TO HOST, CONNECTION REFUSED"; break;
-
-        case E_SET_TIME: return "UNABLE TO SET TIME"; break;
-
-        case E_PARSING: return "PARSING ERROR"; break;
-
-        case E_LOG: return "UNABLE TO LOG"; break;
-
-        default: return "UNKNOW ERROR"; break;
-    }
-}
+extern const char *errorDescription(exit_number exit_number);
 
 #endif
