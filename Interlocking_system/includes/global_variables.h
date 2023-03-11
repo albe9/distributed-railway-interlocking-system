31cf345870d83bb27b48f439f6a0fe993861561d
albe9 <albertom1999@gmail.com>
2023-03-11 18:20:25 +0100

    Definite lunghezze code di messaggi

--- Interlocking_system/includes/global_variables.h
+++ Interlocking_system/includes/global_variables.h
@@ -39,8 +39,14 @@ TASK_ID CONTROL_TID;
 
 SEM_ID GLOBAL_SEM;                //semaforo per gestire l'accesso alle variabili globali condivise da più task
 
+MSG_Q_ID CONTROL_QUEUE;
+
 #define MAX_CONN 50               //numero massimo di connessioni per un nodo
 #define TAIL_ID -9999             //Id associato al nodo successivo all'ultimo nodo di una route
+#define MAX_LOG_SIZE 1024
+#define MAX_LOG_BUFF 10
+#define MAX_CTRL_SIZE 1024      
+#define MAX_CTRL_BUFF 10
 
 
 extern int CURRENT_HOST;          //host corrente che ha avviato il two-phase-commit-protocol
@@ -48,6 +54,6 @@ extern int SERVER_PORT;
 extern int RASP_ID;
 extern char HOST_IP[20];
 extern char RASP_IP[20];
-extern MSG_Q_ID CONTROL_QUEUE;
+
 
 #endif /* INCLUDES_GLOBAL_VARIABLES_H_ */
