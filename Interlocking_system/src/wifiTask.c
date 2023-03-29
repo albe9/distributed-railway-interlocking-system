376a28df535942290de3c23fe39a7e74d38b0aa1
Jacopo Damerini <76707805+jcpdmr@users.noreply.github.com>
2023-03-29 19:29:19 +0200

    Sviluppo threading

--- Interlocking_system/src/wifiTask.c
+++ Interlocking_system/src/wifiTask.c
@@ -229,7 +229,7 @@ int getSizeofLog(char *path_to_file){
 	if((file = fopen(path_to_file, "r")) < 0){
 		logMessage("Problema nell'apertura del file", taskName(0));
 		return E_LOG_OPEN;
-	}
+	}	
 	if (file != NULL) {
     	/* Go to the end of the file. */
 		if (fseek(file, 0L, SEEK_END) == 0) {
@@ -283,13 +283,16 @@ exit_number logToHost(void){
 		if (close(host_conn.sock) < 0){
 			return E_DEFAUL_ERROR;
 		}
+		taskResume(LOG_TID);
 		return E_SUCCESS;
 	}
 	else{
 		// Errore da gestire
+		taskResume(LOG_TID);
 		logMessage("Size del log nulla o negativa [errore]", taskName(0));
 		return E_LOG_EMPTY;
 	}
+
 }
 
 int getSizeofLog(char *path_to_file){
