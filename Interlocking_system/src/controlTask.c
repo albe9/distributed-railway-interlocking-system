b7fc5cfc9642ae2654b571f7d2db402a6277db32
albe9 <albertom1999@gmail.com>
2023-03-31 11:09:41 +0200

    Varie aggiunte : Implementazione dell'analisi dei messaggi del tpcp Gestione dei messaggi negativi Gestione della cancellazione del task (tramite hook) Gestione dello stato del nodo

--- Interlocking_system/src/controlTask.c
+++ Interlocking_system/src/controlTask.c
@@ -1,9 +1,15 @@
 #include "controlTask.h"
 
+static tpcp_status NODE_STATUS=NOT_RESERVED;
+static int HOST_ID = 0;    //per adesso gestiamo un solo host
+static route* current_route = NULL;
 
+void forwardMsg(tpcp_msg* msg, int receiver_id, char* command){
 
-extern void forwardMsg(tpcp_msg* msg, int receiver_id, char* command){
-
+    // debug
+    // char log_msg[100];
+    // snprintf(log_msg, 100, "forwardMsg, command :%s sender :%i recivier:%i route:%i", msg->command, msg->sender_id, msg->recevier_id, msg->route_id);
+    // logMessage(log_msg, taskName(0));
 
     if(command != NULL){
         memset(msg->command , 0, sizeof(msg->command));
@@ -12,28 +18,76 @@ extern void forwardMsg(tpcp_msg* msg, int receiver_id, char* command){
 
     msg->recevier_id = receiver_id;
     msg->sender_id = RASP_ID;
+
+    logMessage("[t4] inoltro il messaggio al next o prev", taskName(0));
+    logMessage("[t5] acquisisco semaforo per la coda", taskName(0));
     msgQSend(OUT_CONTROL_QUEUE, (char*)msg, sizeof(tpcp_msg), WAIT_FOREVER, MSG_PRI_NORMAL);
-    
+    logMessage("[t28] sposto messaggio dalla coda locale a quella globale", taskName(0));
 }
+
+void handleMsg(tpcp_msg* msg, bool direction, tpcp_status new_middleStatus, tpcp_status new_edgeStatus, char* new_command){
+
+    // direction=true indica che i messaggi in questa fase del tpcp si stanno propagando dalla testa verso la coda della route
+    // direction=false il contrario
+    // new_command è il nuovo comando che dovrà inoltrare il nodo all'edge di una route (o il primo o l'ultimo in base alla fase del tpcp)
+    // i nodi intermedi inoltrano semplicemente il comando precedente
+    logMessage("[t0] processo il messaggio", taskName(0));
+
+    if(direction){
+        if(current_route->rasp_id_next != TAIL_ID){
+            // debug
+            // char log_msg[100];
+            // snprintf(log_msg, 100, "handleMsg, command :%s sender :%i recivier:%i route:%i", msg->command, msg->sender_id, msg->recevier_id, msg->route_id);
+            // logMessage(log_msg, taskName(0));
+            NODE_STATUS = new_middleStatus;
+            logMessage("[t1] setto lo stato", taskName(0));
+            forwardMsg(msg, current_route->rasp_id_next, NULL);
+        }
+        else{
+            NODE_STATUS = new_edgeStatus;
+            logMessage("[t1] setto lo stato", taskName(0));
+            forwardMsg(msg, current_route->rasp_id_prev, new_command);
+        }
+    }
+    else{
+        if(current_route->rasp_id_prev != HOST_ID){
+            NODE_STATUS = new_middleStatus;
+            logMessage("[t1] setto lo stato", taskName(0));
+            forwardMsg(msg, current_route->rasp_id_prev, NULL);
+        }
+        else{
+            NODE_STATUS = new_edgeStatus;
+            logMessage("[t1] setto lo stato", taskName(0));
+            if(NODE_STATUS == TRAIN_IN_TRANSITION){
+                forwardMsg(msg, current_route->rasp_id_prev, new_command);
+            }
+            else{
+                forwardMsg(msg, current_route->rasp_id_next, new_command);
+            }
+        }
+    }
+
+}
+
 
 void controlMain(void){
 
     tpcp_msg in_msg;
-    tpcp_status NODE_STATUS=NOT_RESERVED;
-    int HOST_ID = 0;    //per adesso gestiamo un solo host
 
     while(true){
+        logMessage("[t27] acquisisco semaforo per la coda", taskName(0));
         ssize_t byte_recevied = msgQReceive(IN_CONTROL_QUEUE, (char*)&in_msg, sizeof(tpcp_msg), WAIT_FOREVER);
+        logMessage("[t7] sposto semaforo dalla coda globale a quella locale", taskName(0));
+
 
         char msg[100];
         snprintf(msg, 100, "command :%s sender :%i recivier:%i route:%i", in_msg.command, in_msg.sender_id, in_msg.recevier_id, in_msg.route_id);
         logMessage(msg, taskName(0));
 
 
-        // forwardMsg(&in_msg, 2, "TEST");
+        // resetto la rotta precedente e verifico che il nodo appartenga a quella descritta nel messaggio
+        current_route = NULL;
 
-        
-        route* current_route = NULL;
         for(int route_idx=0; route_idx<route_count; route_idx++){
             if(node_routes[route_idx].route_id == in_msg.route_id){
                 current_route = &node_routes[route_idx];
@@ -43,6 +97,8 @@ void controlMain(void){
 
         if( current_route == NULL){
             // TODO gestire risposta quando la route non è presente
+            forwardMsg(&in_msg, in_msg.sender_id, "NOT_OK");
+            resetNodeStatus();
         }
         else{
 
@@ -54,104 +110,96 @@ void controlMain(void){
                 // -REQ
                 // -WAIT_ACK
                 // -ACK
-                // -NACK
 
                 // -WAIT_COMMIT
                 // -COMMIT
-                // -NOT_COMMIT
 
                 // -WAIT_AGREE
                 // -AGREE
-                // -DISAGREE
+
+                // i messaggi negativi potrebbero essere tutti raccolti in unico comando per semplificare la gestione
+                // -NOT_OK
 
             
             //TODO gestire tutti i msg ricevuti in uno stato che non li supporta
-            if (strcmp(in_msg.command, "REQ") == 0){
-                if(NODE_STATUS != NOT_RESERVED){
-                    //TODO gestire
+
+            switch (NODE_STATUS)
+            {
+            case NOT_RESERVED:
+                if (strcmp(in_msg.command, "REQ") == 0){
+                    handleMsg(&in_msg, true, WAIT_ACK, WAIT_COMMIT, "ACK");
                 }
                 else{
+                    // TODO gestire messaggi sbagliati
+                }
+                break;
+            case WAIT_ACK:
+                if (strcmp(in_msg.command, "ACK") == 0){
+                    handleMsg(&in_msg, false, WAIT_COMMIT, WAIT_AGREE, "COMMIT");
+                }
+                else if (strcmp(in_msg.command, "NOT_OK") == 0){
+                    resetNodeStatus();
+                    forwardMsg(&in_msg, current_route->rasp_id_prev, NULL);
+                    
+                }
+                else{
+                    // TODO gestire messaggi sbagliati
+                }
+                break;
+            case WAIT_COMMIT:
+                if (strcmp(in_msg.command, "COMMIT") == 0){
+                    handleMsg(&in_msg, true, WAIT_AGREE, RESERVED, "AGREE");
+                }
+                else if (strcmp(in_msg.command, "NOT_OK") == 0){
+                    resetNodeStatus();
                     if(current_route->rasp_id_next != TAIL_ID){
-                        NODE_STATUS = WAIT_ACK;
                         forwardMsg(&in_msg, current_route->rasp_id_next, NULL);
-                        // in_msg.recevier_id = current_route->rasp_id_next;
-                        // msgQSend(OUT_CONTROL_QUEUE, (char*)&in_msg, sizeof(tpcp_msg), WAIT_FOREVER, MSG_PRI_NORMAL);
                     }
                     else{
-                        NODE_STATUS = WAIT_COMMIT;
-                        forwardMsg(&in_msg, current_route->rasp_id_prev, "ACK");
-                        // memset(in_msg.command , 0, sizeof(in_msg.command));
-                        // strcpy(in_msg.command, "ACK");
-                        // in_msg.recevier_id = current_route->rasp_id_prev;
-                        // msgQSend(OUT_CONTROL_QUEUE, (char*)&in_msg, sizeof(tpcp_msg), WAIT_FOREVER, MSG_PRI_NORMAL);
+                        forwardMsg(&in_msg, HOST_ID, NULL);
                     }
-                }
-            }
-            else if (strcmp(in_msg.command, "ACK") == 0){
-                if(NODE_STATUS != WAIT_ACK){
-                    //TODO gestire 
                 }
                 else{
-                    if(current_route->rasp_id_prev != HOST_ID){
-                        NODE_STATUS = WAIT_COMMIT;
-                        forwardMsg(&in_msg, current_route->rasp_id_prev, NULL);
-                        // in_msg.recevier_id = current_route->rasp_id_prev;
-                        // msgQSend(OUT_CONTROL_QUEUE, (char*)&in_msg, sizeof(tpcp_msg), WAIT_FOREVER, MSG_PRI_NORMAL);
-                    }
-                    else{
-                        NODE_STATUS = WAIT_AGREE;
-                        forwardMsg(&in_msg, current_route->rasp_id_next, "COMMIT");
-                        // memset(in_msg.command , 0, sizeof(in_msg.command));
-                        // strcpy(in_msg.command, "COMMIT");
-                        // in_msg.recevier_id = current_route->rasp_id_next;
-                        // msgQSend(OUT_CONTROL_QUEUE, (char*)&in_msg, sizeof(tpcp_msg), WAIT_FOREVER, MSG_PRI_NORMAL);
-                    }
+                    // TODO gestire messaggi sbagliati
                 }
-            }
-            else if (strcmp(in_msg.command, "COMMIT") == 0){
-                if(NODE_STATUS != WAIT_COMMIT){
-                    //TODO gestire
+                break;
+            case WAIT_AGREE:
+                if (strcmp(in_msg.command, "AGREE") == 0){
+                    if(NODE_TYPE == TYPE_SWITCH){
+                        // TODO verificare il positioning
+                    }
+                    handleMsg(&in_msg, false, RESERVED, TRAIN_IN_TRANSITION, "TRAIN_OK");
+                }
+                else if (strcmp(in_msg.command, "NOT_OK") == 0){
+                    resetNodeStatus();
+                    forwardMsg(&in_msg, current_route->rasp_id_prev, NULL);
                 }
                 else{
-                    if(current_route->rasp_id_next != TAIL_ID){
-                        NODE_STATUS = WAIT_AGREE;
-                        forwardMsg(&in_msg, current_route->rasp_id_next, NULL);
-                        // in_msg.recevier_id = current_route->rasp_id_next;
-                        // msgQSend(OUT_CONTROL_QUEUE, (char*)&in_msg, sizeof(tpcp_msg), WAIT_FOREVER, MSG_PRI_NORMAL);
-                    }
-                    else{
-                        NODE_STATUS = RESERVED;
-                        forwardMsg(&in_msg, current_route->rasp_id_prev, "AGREE");
-                        // memset(in_msg.command , 0, sizeof(in_msg.command));
-                        // strcpy(in_msg.command, "AGREE");
-                        // in_msg.recevier_id = current_route->rasp_id_prev;
-                        // msgQSend(OUT_CONTROL_QUEUE, (char*)&in_msg, sizeof(tpcp_msg), WAIT_FOREVER, MSG_PRI_NORMAL);
-                    }
+                    // TODO gestire messaggi sbagliati
                 }
-            }
-            else if (strcmp(in_msg.command, "ACK") == 0){
-                if(NODE_STATUS != WAIT_ACK){
-                    //TODO gestire 
-                }
-                else{
-                    if(current_route->rasp_id_prev != HOST_ID){
-                        NODE_STATUS = WAIT_COMMIT;
-                        forwardMsg(&in_msg, current_route->rasp_id_prev, NULL);
-                        // in_msg.recevier_id = current_route->rasp_id_prev;
-                        // msgQSend(OUT_CONTROL_QUEUE, (char*)&in_msg, sizeof(tpcp_msg), WAIT_FOREVER, MSG_PRI_NORMAL);
-                    }
-                    else{
-                        NODE_STATUS = WAIT_AGREE;
-                        forwardMsg(&in_msg, current_route->rasp_id_next, );
-                        // memset(in_msg.command , 0, sizeof(in_msg.command));
-                        // strcpy(in_msg.command, "COMMIT");
-                        // in_msg.recevier_id = current_route->rasp_id_next;
-                        // msgQSend(OUT_CONTROL_QUEUE, (char*)&in_msg, sizeof(tpcp_msg), WAIT_FOREVER, MSG_PRI_NORMAL);
-                    }
-                }
+                break;
+            case RESERVED:
+                
+                break;
+            default:
+                break;
             }
         }
     }
 }
 
+void hookControlDelete(_Vx_TASK_ID tid){
+    if(tid == CONTROL_TID){
+        resetNodeStatus();
+    }
+}
 
+void resetNodeStatus(){
+    //resetto lo stato del nodo
+    logMessage("[t0] processo il messaggio", taskName(0));
+    NODE_STATUS=NOT_RESERVED;
+    semTake(GLOBAL_SEM, WAIT_FOREVER);
+    CURRENT_HOST = -1;
+    semGive(GLOBAL_SEM);
+    logMessage("[t1] setto lo stato", taskName(0));
+}
