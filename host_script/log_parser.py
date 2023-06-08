5467c2b4ac902bb473cfaefb879eb9efdb9006bb
albe9 <albertom1999@gmail.com>
2023-06-08 19:06:17 +0200

    Bug fix

--- host_script/log_parser.py
+++ host_script/log_parser.py
@@ -1,5 +1,6 @@
 import re
 import datetime
+import sys
 
 for log_idx in range(1,6):
     log_path = "./log_test/log_192.168.1.21" + str(log_idx) + ".txt"
@@ -10,22 +11,23 @@ for log_idx in range(1,6):
         prev_time = None
         for line in extract:
             line=line.strip()
-           
+
             match = re.search("(\d*h:\d*m:\d*s:\d*ms).*\[(t\d*)\]",line)
+            
             if match is not None:
                 time_str = match.group(1)
                 current_time = datetime.datetime.strptime(time_str, "%Hh:%Mm:%Ss:%fms").time()
                 current_time_td = datetime.timedelta(hours=current_time.hour,
                                                     minutes=current_time.minute,
                                                     seconds=current_time.second,
-                                                    milliseconds=current_time.microsecond)
+                                                    milliseconds=current_time.microsecond/1000)
                 if prev_time is None:
                     prev_time = current_time_td
-                else:
-                    result = current_time_td - prev_time
-                    prev_time = current_time_td
+                
+                result = current_time_td - prev_time
+                prev_time = current_time_td
 
-                    result_ms = int(result.total_seconds() * 1000)
+                result_ms = int(result.total_seconds() * 1000)
 
-                    thread_name = match.group(2)
-                    f.write(f"{thread_name}\n{result_ms}\n")
+                thread_name = match.group(2)
+                f.write(f"{thread_name}\n{result_ms}\n")
