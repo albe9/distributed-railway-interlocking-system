ea6768f5ce60e2a0eec575bc8990081f7b46952e
albe9 <albertom1999@gmail.com>
2023-06-25 18:12:11 +0200

    aggiunte le deadline al tipo 2

--- host_script/log_parser.py
+++ host_script/log_parser.py
@@ -2,6 +2,81 @@ import re
 import datetime
 import sys
 
+"""
+Creare un documento di tipo "File Importer Box" e importare nel progetto
+due file di testo:
+1) il log (RTAI_relative_millisec_1.txt)
+2) la definizione del task-set (task_set_def.txt).
+
+Circa il formato dei file di tipo 1:
+t3 -> prima transizione che esegue
+40 -> tempo relativo di firing della prima transizione (misurato dal
+firing precedente)
+t0 -> seconda transizione che esegue
+0  -> tempo relativo di firing della seconda transizione (misurato dal
+firing precedente)
+t4 -> terza transizione che esegue
+1  -> tempo relativo di firing della terza transizione (misurato dal
+firing precedente)
+...
+
+Circa il formato dei file di tipo 2:
+PROCESSES: 5 -> numero di task
+process1TransitionsNumber: 3 -> numero di transizioni del primo task
+t0 -> prima transizione del primo task
+t1 -> seconda transizione del primo task
+t2 -> terza transizione del primo task
+deadline: 40 ->  -> deadline del primo task
+process2TransitionsNumber: 3
+t3
+t4
+t5
+deadline: 40
+process3TransitionsNumber: 6
+t6
+t7
+t8
+t9
+t10
+t11
+deadline: 80
+process4TransitionsNumber: 8
+t12
+t13
+t14
+t15
+t16
+t17
+t18
+t19
+deadline: 100
+process5TransitionsNumber: 5
+t20
+t21
+t22
+t23
+t24
+deadline: 120
+CONTROLLABLE_TRANSITIONS: 0 -> lasciare zero
+ALIAS_IDENTIFIERS: 0 -> lasciare zero
+
+Quando si crea un documento del plugin "PetriSimulator", si devono
+fornire in ingresso i seguenti file:
+- modello PTPN (PTPN/PTPN.pef.xml)
+- grafo delle classi di stato in formato xml (TCAnalyzer/output.xml)
+- grafo delle classi di stato in formato txt (TCAnalyzer/output.txt)
+- grafo delle classi di stato in formato graphml (TCAnalyzer/output.graphml)
+- definizione del task-set (File Importer Box/task_set_def.txt)
+- log (File Importer Box/RTAI_relative_millisec_1.txt)
+
+Una volta completata la simulazione del log, sono prodotti vari file. In
+particolare:
+- ProfilingMeasure.txt riporta gli istogrammi dei tempi di esecuzione di
+ciascuna transizione
+- ReleaseCompletionAnalysis.txt indica se sono state mancate deadline
+- CoverageMeasure.txt riporta il numero di nodi e archi coperti nel
+grafo delle marcature e in quello delle classi
+"""
 
 TASK_DEADLINES = {
     "ctrlTask" : 100,
@@ -61,5 +136,8 @@ for log_idx in range(1,6):
             f.write(f"process{task_number + 1}TransitionsNumber: {len(transitions)}\n")
             for transition in transitions:
                 f.write(f"{transition}\n")
+            f.write(f"deadline: {TASK_DEADLINES[task_name]}\n")
+        f.write("CONTROLLABLE_TRANSITIONS: 0\n")
+        f.write("ALIAS_IDENTIFIERS: 0\n")
         # print(task_transictions_dict)
     
