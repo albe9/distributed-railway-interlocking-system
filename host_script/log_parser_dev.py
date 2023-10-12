5242f1b55ca7deef59914828718e9fbeb0e79e63
albe9 <albertom1999@gmail.com>
2023-10-12 20:52:51 +0200

    Unificati i layout dei grafici

--- host_script/log_parser_dev.py
+++ host_script/log_parser_dev.py
@@ -139,103 +139,195 @@ for log_idx in range(1,6):
 
 def draw_transition_times():
 
-    # Create a list where element 0 has all the time of executions of transition t0 
-    # for example the list will be [[0,0,0], [16,17,16,0], ...]
-    transitions = [["-"] for _ in range(N_TOTAL_TRANSITIONS + 1)]
-    prev_time = None
+    for log_idx in range(1,6):
+        # Extract the info from the logs
+        log_path = f"{BASE_DIR}/connect/execution_log_files/log_192.168.1.21" + str(log_idx) + ".txt"
+        with open(log_path, "r") as f:
+            extract = f.readlines()
 
-    countings = {}
-    for line in extract:
-        line=line.strip()
+        # Create a list where element 0 has all the time of executions of transition t0 
+        # for example the list will be [[0,0,0], [16,17,16,0], ...]
+        transitions = [["-"] for _ in range(N_TOTAL_TRANSITIONS + 1)]
+        prev_time = None
 
-        # match = re.search("(\d*h:\d*m:\d*s:\d*ms).*\[(t\d*)\]",line)
-        match = re.search("(\d+)    .*\[t(\d+)\]",line)
-        
-        if match is not None:
-            current_time_micro = int(match.group(1))
-            if prev_time is None:
+        countings = {}
+        for line in extract:
+            line=line.strip()
+
+            # match = re.search("(\d*h:\d*m:\d*s:\d*ms).*\[(t\d*)\]",line)
+            match = re.search("(\d+)    .*\[t(\d+)\]",line)
+            
+            if match is not None:
+                current_time_micro = int(match.group(1))
+                if prev_time is None:
+                    prev_time = current_time_micro
+                
+                result = current_time_micro - prev_time
                 prev_time = current_time_micro
-            
-            result = current_time_micro - prev_time
-            prev_time = current_time_micro
 
 
-            result = round(result / 1000)
-            trans_num = int(match.group(2))
+                result = round(result / 1000)
+                trans_num = int(match.group(2))
 
-            if result in countings:
-                countings[result] += 1
-            else:
-                countings[result] = 1
-            
-            # Check if it's the first time that we encounter a transition
-            if transitions[trans_num][0] == "-":
-                # If first time create a list with a single value
-                transitions[trans_num] = [result]
-            elif isinstance(transitions[trans_num][0], int):
-                # If not first time append to the list
-                transitions[trans_num].append(result)
+                if result in countings:
+                    countings[result] += 1
+                else:
+                    countings[result] = 1
+                
+                # Check if it's the first time that we encounter a transition
+                if transitions[trans_num][0] == "-":
+                    # If first time create a list with a single value
+                    transitions[trans_num] = [result]
+                elif isinstance(transitions[trans_num][0], int):
+                    # If not first time append to the list
+                    transitions[trans_num].append(result)
 
-    # Compute statistics
-    max_execution_times = {}        
-    for idx, transition in enumerate(transitions):
-        # Check if transition has data
-        if transition[0] != "-":
-            max_time = max(transition)
-            min_time = min(transition)
-            avg_time = sum(transition) / len(transition)
-            # print(f"t{idx:02d} --->     Max: {max_time:04d}     Min:{min_time:04d}     Avg:{avg_time:07.2f}")
-            if max_execution_times.get(max_time) == None:
-                max_execution_times[max_time] = []
-            max_execution_times[max_time].append(f"t{idx}")
-    max_execution_times = dict(sorted(max_execution_times.items()))        
+        # Compute statistics
+        max_execution_times = {}        
+        for idx, transition in enumerate(transitions):
+            # Check if transition has data
+            if transition[0] != "-":
+                max_time = max(transition)
+                min_time = min(transition)
+                avg_time = sum(transition) / len(transition)
+                # print(f"t{idx:02d} --->     Max: {max_time:04d}     Min:{min_time:04d}     Avg:{avg_time:07.2f}")
+                if max_execution_times.get(max_time) == None:
+                    max_execution_times[max_time] = []
+                max_execution_times[max_time].append(f"t{idx}")
+        max_execution_times = dict(sorted(max_execution_times.items()))        
 
-    # Create max graph
-    times = []
-    occurrencies = []        
-    for time, trans_list in max_execution_times.items():
-        times.append(time)
-        occurrencies.append(len(trans_list))
+        # Create max graph
+        times = []
+        occurrencies = []        
+        for time, trans_list in max_execution_times.items():
+            times.append(time)
+            occurrencies.append(len(trans_list))
+        
+        n = len(times)
+        colors = list(mcolors.TABLEAU_COLORS.values())
+        colors = colors[:n] if n <= len(colors) else colors * (n // len(colors)) + colors[:n % len(colors)]
+        plt.style.use("ggplot")
+        fig, ax = plt.subplots(1, 2, sharey=True)
+        fig.set_size_inches(14,10)
+        fig.subplots_adjust(wspace=0.05)
+
+        x_time_sep = 40
+        times_occurrencies_left = [[t[0] for t in zip(times, occurrencies) if t[0] <= x_time_sep],
+                                   [t[1] for t in zip(times, occurrencies) if t[0] <= x_time_sep]]
+        times_occurrencies_right = [[t[0] for t in zip(times, occurrencies) if t[0] > x_time_sep],
+                                   [t[1] for t in zip(times, occurrencies) if t[0] > x_time_sep]]
+        left_colors = colors[:len(times_occurrencies_left[0])]
+        right_colors = colors[len(times_occurrencies_left[0]):]
+
+        bar_container = []
+        bar_container.append(ax[0].bar(times_occurrencies_left[0],
+                                       times_occurrencies_left[1],
+                                        color=left_colors, width=0.5, log=True))
+        bar_container.append(ax[1].bar(times_occurrencies_right[0],
+                                       times_occurrencies_right[1],
+                                        color=right_colors, width=1.5, log=True))
+        
+        for bar, occurrency, num in zip(bar_container[0], times_occurrencies_left[1], range(0, n)):
+            ax[0].text(bar.get_x() + bar.get_width()/10, np.sqrt(bar.get_height()) if bar.get_height() != 1 else 0.9, str(occurrency),
+                    ha='left', va='center', rotation='vertical', fontsize=6)    
+        
+        for bar, occurrency, num in zip(bar_container[1], times_occurrencies_right[1], range(0, n)):
+            ax[1].text(bar.get_x() + bar.get_width()/10, np.sqrt(bar.get_height()) if bar.get_height() != 1 else 0.9, str(occurrency),
+                    ha='left', va='center', rotation='vertical', fontsize=6)
+
+
+        fig.suptitle("Max execution times [ms]", fontsize=20 ,fontweight='bold')
+        ax[0].annotate('ms', xy=(0.98, -0.07), xycoords='axes fraction',
+             horizontalalignment='right', verticalalignment='center', fontsize=12)
+        
+        ax[0].annotate("Occurrencies", xy=(-0.1, 0.5), xycoords='axes fraction',
+             horizontalalignment='right', verticalalignment='center', fontsize=12, rotation=90)
+        
+        ax[0].tick_params(axis='x', labelsize=6)
+        ax[0].set_xticks(times_occurrencies_left[0])
+        ax[1].tick_params(axis='x', labelsize=6)
+        ax[1].set_xticks(times_occurrencies_right[0])
+
+        ax[0].set_xlim(-1, x_time_sep + 1)
+        ax[1].set_xlim(x_time_sep, max(times_occurrencies_right[0]) + 5)
+
+        # hide the spines between ax and ax2
+        ax[0].spines.right.set_visible(False)
+        ax[1].spines.left.set_visible(False)
+        ax[1].tick_params(which="both", left=False)
+
+        d = .5  # proportion of vertical to horizontal extent of the slanted line
+        kwargs = dict(marker=[(-1, -d), (1, d)], markersize=12,
+                    linestyle="none", color='k', mec='k', mew=1, clip_on=False)
+        ax[0].plot([1, 1], [0, 1], transform=ax[0].transAxes, **kwargs)
+        ax[1].plot([0, 0], [0, 1], transform=ax[1].transAxes, **kwargs)
+
+        fig.savefig(f'{analysis_path}/transitions_max_192.186.1.21{log_idx}.png', dpi=300)
+
+
+
+        # Create execution times graph
+        countings = dict(sorted(countings.items()))
+        times = list(countings.keys())
+        occurrencies = list(countings.values())
+
+        n = len(times)
+        colors = list(mcolors.TABLEAU_COLORS.values())
+        colors = colors[:n] if n <= len(colors) else colors * (n // len(colors)) + colors[:n % len(colors)]
+
+        plt.style.use("ggplot")
+        fig, ax = plt.subplots(1, 2, sharey=True)
+        fig.set_size_inches(14,10)
+        fig.subplots_adjust(wspace=0.05)
+
+        x_time_sep = 30
+        times_occurrencies_left = [[t[0] for t in zip(times, occurrencies) if t[0] <= x_time_sep],
+                                   [t[1] for t in zip(times, occurrencies) if t[0] <= x_time_sep]]
+        times_occurrencies_right = [[t[0] for t in zip(times, occurrencies) if t[0] > x_time_sep],
+                                   [t[1] for t in zip(times, occurrencies) if t[0] > x_time_sep]]
+        left_colors = colors[:len(times_occurrencies_left[0])]
+        right_colors = colors[len(times_occurrencies_left[0]):]
+
     
-    n = len(times)
-    colors = list(mcolors.TABLEAU_COLORS.values())
-    colors = colors[:n] if n <= len(colors) else colors * (n // len(colors)) + colors[:n % len(colors)]
-    plt.style.use("ggplot")
-    fig, ax = plt.subplots(dpi=288)
-    bar_container = ax.bar(times, occurrencies, color=colors, width=2)
-    ax.set(ylabel="Occurrencies", title="Max execution times [ms]")
-    ax.tick_params(axis='x', labelsize=6)
-    ax.set_xticks(times)
-    fig.savefig(f'{analysis_path}/transitions_max_192.186.1.21{log_idx}.png')
+        bar_container = []
+        bar_container.append(ax[0].bar(times_occurrencies_left[0],
+                                       times_occurrencies_left[1],
+                                        color=left_colors, width=0.5, log=True))
+        bar_container.append(ax[1].bar(times_occurrencies_right[0],
+                                       times_occurrencies_right[1],
+                                        color=right_colors, width=1.5, log=True))
 
-    # Create execution times graph
-    countings = dict(sorted(countings.items()))
-    times = list(countings.keys())
-    occurrencies = list(countings.values())
+        for bar, occurrency, num in zip(bar_container[0], times_occurrencies_left[1], range(0, n)):
+            ax[0].text(bar.get_x() + bar.get_width()/10, np.sqrt(bar.get_height()/2), str(occurrency),
+                    ha='left', va='center', rotation='vertical', fontsize=6)    
+        
+        for bar, occurrency, num in zip(bar_container[1], times_occurrencies_right[1], range(0, n)):
+            ax[1].text(bar.get_x() + bar.get_width()/10, np.sqrt(bar.get_height()/2), str(occurrency),
+                    ha='left', va='center', rotation='vertical', fontsize=6)
 
-    n = len(times)
-    colors = list(mcolors.TABLEAU_COLORS.values())
-    colors = colors[:n] if n <= len(colors) else colors * (n // len(colors)) + colors[:n % len(colors)]
-    plt.style.use("ggplot")
-    fig, ax = plt.subplots(dpi=288)
-    bar_container = ax.bar(times, occurrencies, color=colors, width=2)
+        fig.suptitle("Execution times [ms]", fontsize=20 ,fontweight='bold')
+        ax[0].annotate('ms', xy=(0.98, -0.07), xycoords='axes fraction',
+             horizontalalignment='right', verticalalignment='center', fontsize=12)
+        
+        ax[0].annotate("Occurrencies", xy=(-0.1, 0.5), xycoords='axes fraction',
+             horizontalalignment='right', verticalalignment='center', fontsize=12, rotation=90)
 
-    for bar, occurrency, num in zip(bar_container, occurrencies, range(0, n)):
-        ax.text(bar.get_x() + bar.get_width()/2, 10 if num % 2 == 0 else 20, str(occurrency),
-                ha='center', va='center', rotation='vertical', fontsize=6)
-    ax.set(ylabel="Occurrencies", title="Execution times [ms]")
-    ax.tick_params(axis='x', labelsize=4)
-    ax.set_xticks(times, labels=times, visible=False)
-    tick_labels = times
-    for i, tick_label in enumerate(tick_labels):
-        if i % 2 == 1:  # Se l'indice è dispari, sposta l'etichetta verso il basso
-            trans = transforms.Affine2D().translate(0, -5) + ax.transData
-            ax.text(tick_label, 0, str(tick_label), transform=trans, ha='center', fontsize=6)
-        else:  # Altrimenti, posiziona l'etichetta normalmente
-            trans = transforms.Affine2D().translate(0, -8) + ax.transData
-            ax.text(tick_label, 0, str(tick_label), transform=trans, ha='center', fontsize=6)
-    ax.set_ylim(0, 100)
-    fig.savefig(f'{analysis_path}/transitions_exec_192.186.1.21{log_idx}.png')
+
+        ax[0].set_xlim(-1, x_time_sep + 1)
+        ax[1].set_xlim(x_time_sep, max(times_occurrencies_right[0]) + 5)
+
+        # hide the spines between ax and ax2
+        ax[0].spines.right.set_visible(False)
+        ax[1].spines.left.set_visible(False)
+        ax[1].tick_params(which="both", left=False)
+
+        d = .5  # proportion of vertical to horizontal extent of the slanted line
+        kwargs = dict(marker=[(-1, -d), (1, d)], markersize=12,
+                    linestyle="none", color='k', mec='k', mew=1, clip_on=False)
+        ax[0].plot([1, 1], [0, 1], transform=ax[0].transAxes, **kwargs)
+        ax[1].plot([0, 0], [0, 1], transform=ax[1].transAxes, **kwargs)
+
+        fig.savefig(f'{analysis_path}/transitions_exec_192.186.1.21{log_idx}.png', dpi=300)
 
 
 def draw_task_execution_times():
@@ -326,13 +418,14 @@ def draw_task_execution_times():
 
         axs[0].annotate('ms', xy=(0.98, -0.07), xycoords='axes fraction',
              horizontalalignment='right', verticalalignment='center', fontsize=12)
-        axs[0].set_ylabel("Occurences", fontsize=12)
+        axs[0].annotate("Occurrencies", xy=(-0.1, 0.5), xycoords='axes fraction',
+             horizontalalignment='right', verticalalignment='center', fontsize=12, rotation=90)
 
         plt.legend()
-        plt.savefig(f'{HOST_SCRIPT_DIR}/analysis/task_times_192.186.1.21{log_idx}.png')
+        plt.savefig(f'{HOST_SCRIPT_DIR}/analysis/task_times_192.186.1.21{log_idx}.png', dpi=300)
 
 
         
 
-# draw_transition_times()
+draw_transition_times()
 draw_task_execution_times()
